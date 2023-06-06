c0fca79b8afefc3caeeab88991ebf93d9e93521f
pjione <a41924250@gmail.com>
2023-06-06 19:57:20 +0900

    페이징

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,8 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
@@ -7,4 +10,8 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+
+    Page<Member> findByAge(int age, Pageable pageable);
+    Slice<Member> findSliceByAge(int age, Pageable pageable);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,10 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
@@ -52,5 +56,48 @@ class MemberRepositoryTest {
         long deleted = memberRepository.count();
         assertThat(deleted).isEqualTo(0);
 
+    }
+    @Test
+    void paging(){
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        Member member2 = memberRepository.save(new Member("member2", 10));
+        Member member3 = memberRepository.save(new Member("member3", 10));
+        Member member4 = memberRepository.save(new Member("member4", 10));
+        Member member5 = memberRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(1, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        //when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        Slice<Member> slice = memberRepository.findSliceByAge(age, pageRequest);
+
+        //then
+        List<Member> content = page.getContent();
+        List<Member> content2 = slice.getContent();
+        long totalElements = page.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(2);
+        assertThat(totalElements).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(1);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isFalse();
+        assertThat(page.hasNext()).isFalse();
+
+        assertThat(content2.size()).isEqualTo(2);
+        for (Member member : content2) {
+            System.out.println("asdf : " + member);
+        }
+        assertThat(slice.getNumber()).isEqualTo(1);
+        assertThat(slice.isFirst()).isFalse();
+        assertThat(slice.hasNext()).isFalse();
+        /*int age = 10;
+        int offset = 0;
+        int limit = 3;
+*/
+        //when
+        /*List<Member> members = memberRepository.findByAge(age, offset, limit);
+        long totalCount = memberRepository.totalCount(age);*/
     }
 }
